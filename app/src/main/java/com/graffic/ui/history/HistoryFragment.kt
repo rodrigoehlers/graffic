e5f41bef01ae6360883e27b12f07e1b4fffec15a
Eike Nils Knopp <eike.knopp@vibrantmedia.com>
2019-12-02 12:22:24 +0100

    listview for config history

--- app/src/main/java/com/graffic/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/graffic/ui/history/HistoryFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.graffic.R
+import com.graffic.data.Config
+import com.graffic.ui.RecyclerAdapter
+import kotlinx.android.synthetic.main.fragment_history.*
 
 class HistoryFragment : Fragment() {
 
@@ -19,8 +22,22 @@ class HistoryFragment : Fragment() {
     ): View? {
         historyViewModel =
             ViewModelProviders.of(this).get(HistoryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_custom, container, false)
+
+
+
+
+
+
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val configs = ArrayList<Config>()
+        configs.add(Config(1, 1, 1, 1, 1, 1))
+        configs.add(Config(2, 2, 2, 2, 2, 2))
+        history_list.adapter = RecyclerAdapter(configs, this.requireContext())
     }
 }
